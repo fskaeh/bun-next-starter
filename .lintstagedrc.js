b9a5df0430d50cbdd3407e2caa5c9f4d914c966b
nicolas.stadler <nicolas.stadler@teads.tv>
2023-11-14 21:44:36 +0100

    Improve lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,7 @@
 /** @type {import('lint-staged').Config} */
 const config = {
-  '**/*.{js,jsx,ts,tsx}': [
-    'eslint --fix',
-    'prettier --config ./.prettierrc.js --write',
-  ],
-  '**/*.{css,scss,md,html,json}': [
-    'prettier --config ./.prettierrc.js --write',
-  ],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{css,scss,md,html,json}': ['prettier --write'],
 }
 
 module.exports = config
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,16 @@
 /** @type {import('prettier').Config} */
 const config = {
   semi: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
   printWidth: 80,
-  arrowParens: "always",
+  arrowParens: 'always',
   bracketSpacing: true,
   jsxSingleQuote: true,
   singleAttributePerLine: true,
-  plugins: ["prettier-plugin-tailwindcss"]
+  plugins: ['prettier-plugin-tailwindcss'],
 }
 
 module.exports = config
